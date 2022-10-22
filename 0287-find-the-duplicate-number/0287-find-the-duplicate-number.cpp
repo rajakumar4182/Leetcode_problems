e34ff90130b368c807af5e39d392e6d64f945d30
rajakumar4182 <112674916+rajakumar4182@users.noreply.github.com>
2022-10-22 08:20:44 +0530

    Time: 354 ms (22.97%), Space: 61.3 MB (61.91%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
-        for(int i=0;i<nums.size();i++){
-            int index = abs(nums[i]);
+        // BY modifying array TC = O(n) && SC = O(1)
+//         for(int i=0;i<nums.size();i++){
+//             int index = abs(nums[i]);
             
-            if(nums[index] < 0) return index;
-            nums[index] = -nums[index];
+//             if(nums[index] < 0) return index;
+//             nums[index] = -nums[index];
+//         }
+//         return -1;
+        
+        // using circle in linkedlist
+        
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        
+        return fast;
+            
+            
     }
 };
