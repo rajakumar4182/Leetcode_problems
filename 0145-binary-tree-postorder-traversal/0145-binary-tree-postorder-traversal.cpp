8529139443c2ced9a0b5da34db955121d0a54fe9
rajakumar4182 <112674916+rajakumar4182@users.noreply.github.com>
2023-10-31 09:43:56 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (74.12%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,27 +12,58 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        //Using two stacks
+        //Using one stacks
         
         vector<int> ans;
         if(!root) return ans;
-        stack<TreeNode* > s1,s2;
-        
-        s1.push(root);
-        
-        while(!s1.empty()){
-            TreeNode* cur = s1.top();
-            s1.pop();
-            s2.push(cur);
-            
-            if(cur->left) s1.push(cur->left);
-            if(cur->right) s1.push(cur->right);
-        }
-        
-        while(!s2.empty()){
-            ans.push_back(s2.top()->val);
-            s2.pop();
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while(cur!=NULL || !st.empty()){
+            if(cur!=NULL){
+                st.push(cur);
+                cur = cur->left;
+            }
+            else{
+                TreeNode* temp = st.top()->right;
+                if(temp==NULL){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else{
+                    cur = temp;
+                }
+            }
         }
         return ans;
+        
+        
+        //Using two stacks
+        
+//         vector<int> ans;
+//         if(!root) return ans;
+//         stack<TreeNode* > s1,s2;
+        
+//         s1.push(root);
+        
+//         while(!s1.empty()){
+//             TreeNode* cur = s1.top();
+//             s1.pop();
+//             s2.push(cur);
+            
+//             if(cur->left) s1.push(cur->left);
+//             if(cur->right) s1.push(cur->right);
+//         }
+        
+//         while(!s2.empty()){
+//             ans.push_back(s2.top()->val);
+//             s2.pop();
+//         }
+//         return ans;
     }
 };
