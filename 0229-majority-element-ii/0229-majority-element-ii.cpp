0e01ff219c0e848ec01fa2f287e7fc3861d8e1f8
rajakumar4182 <112674916+rajakumar4182@users.noreply.github.com>
2024-05-03 08:07:05 +0530

    Time: 9 ms (57.26%), Space: 18.2 MB (91.64%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        unordered_map<int,int> m;
-        for(auto it: nums){
-            m[it]++;
+    vector<int> ans;
+    int candidate1 = 0, candidate2 = 0;
+    int count1 = 0, count2 = 0;
+    
+    for (int num : nums) {
+        if (num == candidate1) {
+            count1++;
+        } else if (num == candidate2) {
+            count2++;
+        } else if (count1 == 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 == 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
         }
-        
-        for(auto it=m.begin();it!=m.end();it++){
-            if(it->second > nums.size()/3){
-                ans.push_back(it->first);
-            }
-        }
-        return ans;
     }
+    
+    count1 = 0;
+    count2 = 0;
+    for (int num : nums) {
+        if (num == candidate1) {
+            count1++;
+        } else if (num == candidate2) {
+            count2++;
+        }
+    }
+    
+    if (count1 > nums.size() / 3) {
+        ans.push_back(candidate1);
+    }
+    if (count2 > nums.size() / 3) {
+        ans.push_back(candidate2);
+    }
+    
+    return ans;
+}
+
 };
