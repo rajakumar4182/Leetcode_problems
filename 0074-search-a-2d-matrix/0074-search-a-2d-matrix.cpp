898afe51e6aeaf1b316c0775ba26e53331379331
rajakumar4182 <112674916+rajakumar4182@users.noreply.github.com>
2024-06-29 18:54:57 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (98.56%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        // Edge case for single element matrix
+        if (m == 1 && n == 1) {
+            return matrix[0][0] == target;
+        }
+        
+        int left = 0, right = m * n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n];
+            
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+};
